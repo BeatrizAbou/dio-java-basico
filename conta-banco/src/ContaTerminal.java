65dc9cfa426d16c71cc87023fcd0e79a1ed2dba8
Beatriz_Abou <anabeatriza.said@gmail.com>
2024-09-28 11:54:28 -0300

    commit -message

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner (System.in);
+
+        System.out.println("------------INFORMAÇÕES---------");
+        System.out.println();
+        System.out.println("Olá! Bem vindo ao Banco Guarda Grana! ");
+        System.out.println("Para iniciarmos, informe seu nome e sobrenome");
+        String nomeCliente = scanner.nextLine();
+
+
+
     }
+
+    //Exibir as mensagems para o usuário
+
+    //Obter pela classe scanner os valores digitados no terminal
+
+    //Exibir a mensagem da conta criada
 }
